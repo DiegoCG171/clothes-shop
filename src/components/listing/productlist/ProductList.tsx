f6235b16c60238223a8d4718b0fc5f18be4c3e5f
Helena <velazquez.helena.2@gmail.com>
2025-04-15 13:48:19 -0600

    Corrección de la sección lista de productos

--- src/components/listing/productlist/ProductList.tsx
+++ src/components/listing/productlist/ProductList.tsx
@@ -1,30 +1,42 @@
+import { List, Pagination } from "antd";
+import { BestSellingProductItem } from "../../products/BestSellingProductItem";
+import { Product } from "./ProductListData";
 import { useState } from "react";
-import { Pagination } from "antd";
-import { ProductListItem } from "./ProductListItem";
-import { products } from "./ProductListData";
-import "./ProductList.css";
 
-export const ProductList = () => {
-  const pageSize = 6; // productos por página
+interface ProductListProps {
+  products: Product[];
+}
+
+export const ProductList = ({ products }: ProductListProps) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 6;
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   const startIndex = (currentPage - 1) * pageSize;
   const currentProducts = products.slice(startIndex, startIndex + pageSize);
 
   return (
     <>
-      <div className="product-grid">
-        {currentProducts.map((product, index) => (
-          <ProductListItem key={index} {...product} />
-        ))}
-      </div>
+      <List
+        grid={{
+          gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 3, xxl: 3, }}
+        dataSource={currentProducts}
+        renderItem={(item) => (
+          <List.Item>
+            <BestSellingProductItem {...item} />
+          </List.Item>
+        )}
+      />
 
       <div style={{ display: "flex", justifyContent: "center", marginTop: 24 }}>
         <Pagination
           current={currentPage}
-          pageSize={pageSize}
           total={products.length}
-          onChange={(page) => setCurrentPage(page)}
+          pageSize={pageSize}
+          onChange={handlePageChange}
         />
       </div>
     </>
--- src/components/listing/productlist/ProductListData.ts
+++ src/components/listing/productlist/ProductListData.ts
@@ -2,6 +2,7 @@ import coverblue from "../../../assets/cover-blue.png";
 import coverblack from "../../../assets/cover-black.png";
 import coveratletic from "../../assets/coveratletic.png";
 import Cover0 from "../../assets/cover-0.png";  //si 5
+import Cover1 from "../../assets/cover-1.png";
 import Cover2 from "../../assets/cover-2.png"; //Si 7
 import Cover3 from "../../assets/cover-3.png"; //si 8
 import Cover5 from "../../assets/cover-5.png"; //Si 2
@@ -23,6 +24,7 @@ export const products: Product[] = [
         title: "Classic monochrome Tees",
         inStock: true,
         price: "35.00",
+        isBestSelling: false,
     },
     {
         img: Cover5,
@@ -50,31 +52,43 @@ export const products: Product[] = [
         title: "Elegant Ebony Sweatshirts",
         inStock: true,
         price: "35.00",
+        isBestSelling: false,
     },
     {
         img: coverblack,
         title: "Sleek and Cozy Black",
         inStock: true,
         price: "57.00",
+        isBestSelling: false,
     },
     {
         img: Cover2,
         title: "Raw Black Tees",
         inStock: true,
         price: "19.00",
+        isBestSelling: false,
     },
     {
         img: Cover3,
         title: "MOCKUP Black",
         inStock: true,
         price: "30.00",
+        isBestSelling: false,
     },
     {
         img: coveratletic,
         title: "Athletc Shirt",
         inStock: true,
         price: "35.00",
+        isBestSelling: false,
     },
+    {
+        img: Cover1,
+        title: "Sleek and Cozy Black",
+        inStock: true,
+        price: "57.00",
+        isBestSelling: false,
+      },
 
 
 
